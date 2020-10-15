1c3730e439cc22a6e28e9562ec0f9c4eef8a638d
JackRug <62265588+JackRug@users.noreply.github.com>
2020-10-14 17:09:33 -0700

    powerplant locations and map loaded

--- API_Icon/static/js/logic.js
+++ API_Icon/static/js/logic.js
@@ -1,41 +1,18 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-power-plants-list%40"+
-                "kapsarc&q=united+states&rows=4857&sort=plant_design_capacity_mwe&facet=plant_country&"+
-                "facet=plant_state&facet=plant_status&facet=plant_type_of_ownership&"+
-                "facet=plant_operating_company&facet=type";
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+var queryUrl = "static/resources/world-power-plants-list.geojson";
 
-    console.log(data);
 
-  createFeatures(data);
-});
 
-function createFeatures(data) {
+function createMap(data) {
  
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-//   function onEachFeature(feature, layer) {
-//       console.log(feature);
-//     layer.bindPopup("<h3>" + feature.properties.place +
-//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-//  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-//   var plantData = L.geoJSON(data, {
-
-//     onEachFeature: onEachFeature
-//   });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(data);
-}
-
-function createMap(plantData) {
+  // Function generates a marker on each 
+  function onEachFeature(feature) {
+    console.log(feature);
+    L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]]).addTo(myMap);
+  }
 
   // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -44,37 +21,47 @@ function createMap(plantData) {
     accessToken: API_KEY
   });
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-  });
+  // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  //   maxZoom: 18,
+  //   id: "dark-v10",
+  //   accessToken: API_KEY
+  // });
 
   // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
+  // var baseMaps = {
+  //   "Street Map": streetmap,
+  //   "Dark Map": darkmap
+  // };
 
   // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-//    Plants: plantData
-  };
+  // var overlayMaps = {
+  //   Plants: plantData
+  // };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create myMap, centering on Potwin Kansas
   var myMap = L.map("map", {
     center: [
       38, -97
     ],
-    zoom: 2,
-    layers: [streetmap]
+    zoom: 2
   });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+  // Run onEachFeature function on each datum
+  var plantData = L.geoJSON(data, {
+    onEachFeature: onEachFeature
+  });
+
+  // Add baseMaps
+  satMap.addTo(myMap);
 }
+
+
+// Perform GET request to the query URL (in this case, file saved locally)
+d3.json(queryUrl, function(data) {
+
+  console.log(data);
+
+  // Generate map
+  createMap(data);
+});
