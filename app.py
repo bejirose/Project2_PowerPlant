168f0bb42256d672682d07c0791b6c374a76656e
Poonam Kushwaha <poonam.kushwaha@gamil.com>
2020-10-18 16:37:31 -0700

    all new html pages

--- app.py
+++ app.py
@@ -43,6 +43,30 @@ cursor.execute(countrysql)
 countryRec = cursor.fetchall()
 #print(countryRec)
 
+sql2 = 'select "PLANT_NAME","PLANT_COUNTRY","PLANT_STATE","TYPE" FROM world_plant_list WHERE "TYPE" = %s and "PLANT_NAME" NOT LIKE %s ORDER BY "PLANT_DESIGN_CAPACITY_MWE" DESC FETCH FIRST 20 ROW ONLY'
+
+cursor.execute(sql2, ("COAL","%(Shutdown)"))
+coal_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("GAS","%(Decommissioned)"))
+gas_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("OIL","%(Shutdown)"))
+oil_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("HYDRO","%(Shutdown)"))
+hydro_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("NUCLEAR","%(Shutdown)"))
+nuclear_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("WIND","%(Shutdown)"))
+wind_tables = cursor.fetchall()
+
+cursor.execute(sql2, ("SOLAR_PV","%(Shutdown)"))
+solar_tables = cursor.fetchall()
+
+
 # Set route
 @app.route('/')
 def index():
@@ -139,6 +163,40 @@ def csvdata5():
     # Return the template
     return render_template('data/countries.json')
 
+@app.route('/coal')
+def coal():
+    # Return the template with the teams list passed in
+    return render_template('coal.html', output_data=coal_tables)
+
+@app.route('/gas')
+def gas():
+    # Return the template with the teams list passed in
+    return render_template('gas.html', output_data=gas_tables)
+
+@app.route('/oil')
+def oil():
+    # Return the template with the teams list passed in
+    return render_template('oil.html', output_data=oil_tables)
+
+@app.route('/hydro')
+def hydro():
+    # Return the template with the teams list passed in
+    return render_template('hydro.html', output_data=hydro_tables)
+
+@app.route('/nuclear')
+def nuclear():
+    # Return the template with the teams list passed in
+    return render_template('nuclear.html', output_data=nuclear_tables)
+
+@app.route('/wind')
+def wind():
+    # Return the template with the teams list passed in
+    return render_template('wind.html', output_data=wind_tables)
+
+@app.route('/solar')
+def solar():
+    # Return the template with the teams list passed in
+    return render_template('solar.html', output_data=solar_tables)
 
 #D3.json("/api_data");
 
